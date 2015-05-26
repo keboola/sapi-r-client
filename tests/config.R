d751cb0916a53652ee568d5af72dd43ba88eee2f
pivnicek <marc@marcs-MacBook-Pro.local>
2015-05-26 15:02:56 +0200

    added test environment variable

--- tests/config.R
+++ tests/config.R
@@ -1,4 +1,10 @@
 # replace "your_token" with your keboola connection project token
+# or set environment variable TEST_TOKEN with your token string
+
 apiUrl <- 'https://connection.keboola.com/v2/'
-apitoken <- 'your_token'
+if (nchar(Sys.getenv("TEST_TOKEN")) > 0) {
+  apitoken <- Sys.getenv("TEST_TOKEN")  
+}else {
+  apitoken <- 'your_token'  
+}
 userAgent <- "Keboola StorageApi R Client/v2"

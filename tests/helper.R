8d25678c6e0dd23551d5e738fb1a73b874723d63
Odin <odinuv@gmail.com>
2018-09-03 16:39:56 +0200

    fix: remove leftover

--- tests/helper.R
+++ tests/helper.R
@@ -1,5 +1,3 @@
-Sys.setenv('KBC_TEST_TOKEN' = '5129-146105-iV1iQBFekBNNKbayr6heIfxr5Bo26ZvjaA3Pu7WG', 'KBC_TEST_URL' = 'https://connection.keboola.com/')
-
 var <- Sys.getenv('KBC_TEST_TOKEN')
 if (var == '') {
     stop('KBC_TEST_TOKEN environment variable is empty')

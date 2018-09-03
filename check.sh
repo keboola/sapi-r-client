6559859faeb9b8e9a72057cefdd655cd44a653e8
Odin <odinuv@gmail.com>
2018-09-03 15:23:47 +0200

    chore: fix return value

--- check.sh
+++ check.sh
@@ -4,10 +4,10 @@ if R CMD check "keboola.sapi.r.client_0.4.0.tar.gz" --as-cran --no-manual; then
     echo "Build log:"
     cat /code/keboola.sapi.r.client.Rcheck/tests/testthat.Rout
     echo "Build Succeeded"
-    return 0
+    exit 0
 else
     echo "Error Log:"
     cat /code/keboola.sapi.r.client.Rcheck/tests/testthat.Rout.fail
     echo "Build Failed"
-    return 1
+    exit 1
 fi
